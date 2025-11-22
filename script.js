81cf060375da6f790301c8bfb82e4e9030f0efb5
Ryan <rafaestrada.rafa@gmail.com>
2025-11-22 00:21:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const gallery = document.querySelectorAll('.gallery img');
+const galleryDiv = document.getElementById('gallery');
+
+// Automatically load all images in /images folder
+// You just need to name them jb1.jpg, jb2.jpg, etc.
+const totalImages = 20; // adjust to the total number of images you have
+let galleryImgs = [];
+
+for(let i=1; i<=totalImages; i++){
+    let img = document.createElement('img');
+    img.src = `images/jb${i}.jpg`;
+    img.alt = `JB Photo ${i}`;
+    galleryDiv.appendChild(img);
+    galleryImgs.push(img);
+}
+
+// Modal functionality
 const modal = document.getElementById('modal');
 const modalImg = document.getElementById('modal-img');
 const caption = document.getElementById('caption');
@@ -8,23 +23,23 @@ const nextBtn = document.getElementById('next');
 
 let currentIndex = 0;
 
-gallery.forEach((img, index) => {
-  img.addEventListener('click', () => {
-    modal.style.display = 'block';
-    modalImg.src = img.src;
-    caption.innerText = img.alt;
-    currentIndex = index;
-  });
+galleryImgs.forEach((img, index) => {
+    img.addEventListener('click', () => {
+        modal.style.display = 'block';
+        modalImg.src = img.src;
+        caption.innerText = img.alt;
+        currentIndex = index;
+    });
 });
 
 closeBtn.onclick = () => { modal.style.display = 'none'; }
 
 function showSlide(index) {
-  if(index < 0) index = gallery.length - 1;
-  if(index >= gallery.length) index = 0;
-  modalImg.src = gallery[index].src;
-  caption.innerText = gallery[index].alt;
-  currentIndex = index;
+    if(index < 0) index = galleryImgs.length - 1;
+    if(index >= galleryImgs.length) index = 0;
+    modalImg.src = galleryImgs[index].src;
+    caption.innerText = galleryImgs[index].alt;
+    currentIndex = index;
 }
 
 prevBtn.onclick = () => { showSlide(currentIndex - 1); }
